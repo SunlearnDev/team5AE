94de66badc9ee161128650642814d14a0639acf1
Tai-TC <taitc.1440101226679@vtc.edu.vn>
2023-05-14 10:17:31 +0700

    error

--- ListNhacCaNhan/Singer.cs
+++ ListNhacCaNhan/Singer.cs
@@ -8,15 +8,28 @@ namespace ListNhacCaNhan
 {
     internal class Singer : Human
     {
+        private char hangSinger;
+        
+        public char HangSinger
+        {
+            set { this.HangSinger = value; }
+            get { return this.HangSinger}
         public override void Nhap()
         {
+
             base.Nhap();
+            Console.Write("Nhap hang ca si : ");
+            this.hangSinger = Conver
         }
         public override void Xuat()
         {
             base.Xuat();
         }
         public Singer() : base() { }
-        public Singer(String id, String name, DateTime birth, String gender) : base(id, name, birth, gender) {}
+        public Singer(String id, String name, DateTime birth, String gender, char HangSinger) : base() 
+        {
+           this.hangSinger = hangSinger;
+            
+        }
     }
 }
